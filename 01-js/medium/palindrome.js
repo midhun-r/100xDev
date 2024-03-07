ff50ff53bdb77064b6db4bf1936a666ad23e658c
midhun-r <rmidhun079@gmail.com>
2024-03-08 00:36:04 +0530

    Solved palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,19 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLocaleLowerCase();
+  let strLength = str.length;
+  let firstHalfStartIndex = 0;
+  let firstHalfEndIndex = Math.floor(strLength/2);
+  let lastHalfFirstIndex = Math.ceil(strLength/2);
+  let lastHalfEndIndex = strLength;
+
+  let strFirstHalf = str.slice(firstHalfStartIndex,firstHalfEndIndex).split('').sort().join('');
+  let strLastHalf = str.slice(lastHalfFirstIndex,lastHalfEndIndex).split('').sort().join('');
+  
+  if (strFirstHalf == strLastHalf) {
   return true;
+  }
 }
 
 module.exports = isPalindrome;
